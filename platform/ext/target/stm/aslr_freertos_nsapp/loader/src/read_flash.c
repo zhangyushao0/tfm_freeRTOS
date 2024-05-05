ab85b1f07dd685eaa1a18ba8777fae0b0e5cdebc
zhanghan <zhangh.coder@outlook.com>
2024-05-05 21:13:15 +0800

    TFM_ASLR in main.c

--- platform/ext/target/stm/aslr_freertos_nsapp/loader/src/read_flash.c
+++ platform/ext/target/stm/aslr_freertos_nsapp/loader/src/read_flash.c
@@ -1,5 +1,5 @@
 #include "read_flash.h"
-copy_region_t new_MSIRangeTable = {0x805855c, 0x805859c, 0};
-copy_region_t new_AHBPrescTable = {0x805859c, 0x80585ac, 0};
-copy_region_t new_copy_table = {0x80585bc, 0x80585d4, 0};
-copy_region_t new_zero_table = {0x80585d4, 0x80585e4, 0};
+copy_region_t new_MSIRangeTable = {0x8055e0c, 0x8055e4c, 0};
+copy_region_t new_AHBPrescTable = {0x8055e4c, 0x8055e5c, 0};
+copy_region_t new_copy_table = {0x8055e6c, 0x8055e84, 0};
+copy_region_t new_zero_table = {0x8055e84, 0x8055e94, 0};
--- platform/ext/target/stm/aslr_freertos_nsapp/nsapp/src/main_ns.c
+++ platform/ext/target/stm/aslr_freertos_nsapp/nsapp/src/main_ns.c
@@ -1,10 +1,10 @@
 #include "main_ns.h"
-#include "FreeRTOS.h"
+// #include "FreeRTOS.h"
 #include "stm32l562xx.h"
 #include "stm32l5xx_hal.h"
 #include "stm32l5xx_hal_rcc.h"
 #include "support.h"
-#include "task.h"
+// #include "task.h"
 // #include "trampline.h"
 
 #define TFM_SPM_LOG_LEVEL TFM_SPM_LOG_LEVEL_DEBUG
@@ -27,36 +27,30 @@ static void MX_GPIO_Init(void) {
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
     HAL_GPIO_Init(LED9_GPIO_Port, &GPIO_InitStruct);
 }
-void* ret_addr1;
-void* ret_addr2;
 
-int sum(int a, int b) {
-    return a + b;
-}
-int a = 0;
-int b = 0;
+// void testThread1(void* pvParameters) {
+//     //     SysTick->LOAD = 0xFFFFFFFF; // Set the reload value to the maximum
+//     //     SysTick->VAL = 0;           // Clear the current value to 0
+//     uint32_t start = xTaskGetTickCount();
+//     for (int i = 0; i < 1000; ++i) {
+//         initialise_benchmark();
+//         int result = benchmark();
+//         verify_benchmark(result);
+//     }
+//     uint32_t end = xTaskGetTickCount();
+//     uint32_t res = end - start;
+//     while (1) {
+//         res = end - start;
+//         vTaskDelay(1000);
+//     }
+// }
 
-void testThread1(void* pvParameters) {
-    uint32_t start = xTaskGetTickCount();
-    for (int i = 0; i < 1000; ++i) {
-        initialise_benchmark();
-        int result = benchmark();
-        verify_benchmark(result);
-    }
-    uint32_t end = xTaskGetTickCount();
-    uint32_t res = end - start;
-    while (1) {
-        res = end - start;
-        vTaskDelay(1000);
-    }
-}
-
-void testThread2(void* pvParameters) {
-    while (1) {
-        HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_3);
-        vTaskDelay(1000);
-    }
-}
+// void testThread2(void* pvParameters) {
+//     while (1) {
+//         HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_3);
+//         vTaskDelay(1000);
+//     }
+// }
 
 // void testThread3(void* pvParameters) {
 //     initialise_benchmark();
@@ -67,38 +61,57 @@ void testThread2(void* pvParameters) {
 //     }
 // }
 
+void func() {
+    SysTick->CTRL = 0;          // Disable SysTick
+    SysTick->LOAD = 0xFFFFFFFF; // Set the reload value to the maximum
+    SysTick->VAL = 0;           // Clear the current value to 0
+    SysTick->CTRL = 0x5;
+    initialise_benchmark();
+    int result = benchmark();
+    verify_benchmark(result);
+    uint32_t start = SysTick->VAL;
+    for (int i = 0; i < 1000; ++i) {
+        initialise_benchmark();
+        int result = benchmark();
+        verify_benchmark(result);
+    }
+    uint32_t end = SysTick->VAL;
+    uint32_t res = start - end;
+}
+
 char cArray[128] __attribute__((aligned(128)));
 int  main() {
     HAL_Init();
     MX_GPIO_Init();
+    func();
 
-    BaseType_t xReturned;
+    // BaseType_t xReturned;
 
-    xReturned = xTaskCreate(
-        testThread1,           /* Function that implements the task. */
-        "testThread1",         /* Text name for the task. */
-        ((uint16_t)200),       /* Stack size in words, not bytes. */
-        NULL,                  /* Parameter passed into the task. */
-        1 | portPRIVILEGE_BIT, /* Priority at which the task is created. */
-        NULL);                 /* Used to pass out the created task's handle. */
+    // xReturned = xTaskCreate(
+    //     testThread1,           /* Function that implements the task. */
+    //     "testThread1",         /* Text name for the task. */
+    //     ((uint16_t)200),       /* Stack size in words, not bytes. */
+    //     NULL,                  /* Parameter passed into the task. */
+    //     1 | portPRIVILEGE_BIT, /* Priority at which the task is created. */
+    //     NULL);                 /* Used to pass out the created task's handle. */
 
-    xReturned = xTaskCreate(
-        testThread2,           /* Function that implements the task. */
-        "testThread2",         /* Text name for the task. */
-        ((uint16_t)200),       /* Stack size in words, not bytes. */
-        NULL,                  /* Parameter passed into the task. */
-        1 | portPRIVILEGE_BIT, /* Priority at which the task is created. */
-        NULL);                 /* Used to pass out the created task's handle. */
+    // xReturned = xTaskCreate(
+    //     testThread2,           /* Function that implements the task. */
+    //     "testThread2",         /* Text name for the task. */
+    //     ((uint16_t)200),       /* Stack size in words, not bytes. */
+    //     NULL,                  /* Parameter passed into the task. */
+    //     1 | portPRIVILEGE_BIT, /* Priority at which the task is created. */
+    //     NULL);                 /* Used to pass out the created task's handle. */
 
-    //  xReturned = xTaskCreate(
-    //      testThread3,           /* Function that implements the task. */
-    //      "testThread3",         /* Text name for the task. */
-    //      ((uint16_t)100),       /* Stack size in words, not bytes. */
-    //      NULL,                  /* Parameter passed into the task. */
-    //      1 | portPRIVILEGE_BIT, /* Priority at which the task is created. */
-    //      NULL);
-    /* 启动调度器 */
-    vTaskStartScheduler();
+    // //  xReturned = xTaskCreate(
+    // //      testThread3,           /* Function that implements the task. */
+    // //      "testThread3",         /* Text name for the task. */
+    // //      ((uint16_t)100),       /* Stack size in words, not bytes. */
+    // //      NULL,                  /* Parameter passed into the task. */
+    // //      1 | portPRIVILEGE_BIT, /* Priority at which the task is created. */
+    // //      NULL);
+    // /* 启动调度器 */
+    // vTaskStartScheduler();
 
     /* 如果系统正常工作，以下代码不会执行 */
     for (;;) {
