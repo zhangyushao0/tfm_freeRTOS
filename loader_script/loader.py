36855f6504ca640229b6d7c70a7f925b0d3850dd
jiang-zixian <980485445@qq.com>
2024-05-06 11:43:16 +0800

    fix encode for function pointer

--- loader_script/loader.py
+++ loader_script/loader.py
@@ -202,24 +202,17 @@ def generate_relocation_info(elf_file, symbol_tables, functions_info):
                 func_id1 = get_function_id(functions_info, offset)
                 func_id2 = get_function_id(functions_info, value)
             elif relocation_type == 0x2F:
+                type = 5  # absoultably address: movw
                 func_id1 = get_function_id(functions_info, offset)
                 func_id2 = get_function_id(functions_info, value)
-                if value==functions_info[func_id2][0]:
-                    type=55 #func pointer
-                else:
-                    type = 5  # absoultably address: movw
             elif relocation_type == 0x30:
+                type = 6  # absoulately address: movt
                 func_id1 = get_function_id(functions_info, offset)
                 func_id2 = get_function_id(functions_info, value)
-                if value==functions_info[func_id2][0]:
-                    type=66 #func pointer
-                else:
-                    type = 6  # absoultably address: movw
             else:
                 print(f"error occur: the symbol type not found: {name}")
 
             info.append([offset, value, type,  func_id1, func_id2, name])
-    print(info)
     return info
 
 # 生成函数信息
@@ -249,6 +242,7 @@ def generate_functions_info(symbol_tables):
             # + sections_info[symbol[3]][0]
             global trampoline_blx_size
             trampoline_blx_size = symbol[1]
+
     return functions_info
 
 def output_trampoline():
--- platform/ext/target/stm/aslr_freertos_nsapp/loader/src/loader.c
+++ platform/ext/target/stm/aslr_freertos_nsapp/loader/src/loader.c
@@ -84,7 +84,7 @@ void movw_calculate(relocation_info_t* entry) {
     uint32_t pos = entry->addr - func_info[func_id1].addr + func_info[func_id1].reloc_addr;
     uint32_t val = entry->value;
     if (func_id2 != -1) {
-        val = func_info[func_id2].reloc_addr;
+        val = func_info[func_id2].reloc_addr^ ENCODE_KEY;
     }
     *((uint32_t*)pos) = movw_address_calculate(*(uint32_t*)(entry->addr), val);
 }
@@ -95,29 +95,7 @@ void movt_calculate(relocation_info_t* entry) {
     uint32_t pos = entry->addr - func_info[func_id1].addr + func_info[func_id1].reloc_addr;
     uint32_t val = entry->value;
     if (func_id2 != -1) {
-        val = func_info[func_id2].reloc_addr;
-    }
-    *((uint32_t*)pos) = movt_address_calculate(*(uint32_t*)(entry->addr), val);
-}
-
-void fp_movw_calculate(relocation_info_t* entry) {
-    int      func_id1 = entry->func_id1;
-    int      func_id2 = entry->func_id2;
-    uint32_t pos = entry->addr - func_info[func_id1].addr + func_info[func_id1].reloc_addr;
-    uint32_t val = entry->value^ ENCODE_KEY;
-    if (func_id2 != -1) {
-        val = func_info[func_id2].reloc_addr;
-    }
-    *((uint32_t*)pos) = movw_address_calculate(*(uint32_t*)(entry->addr), val);
-}
-
-void fp_movt_calculate(relocation_info_t* entry) {
-    int      func_id1 = entry->func_id1;
-    int      func_id2 = entry->func_id2;
-    uint32_t pos = entry->addr - func_info[func_id1].addr + func_info[func_id1].reloc_addr;
-    uint32_t val = entry->value^ ENCODE_KEY;
-    if (func_id2 != -1) {
-        val = func_info[func_id2].reloc_addr;
+        val = func_info[func_id2].reloc_addr^ ENCODE_KEY;
     }
     *((uint32_t*)pos) = movt_address_calculate(*(uint32_t*)(entry->addr), val);
 }
@@ -145,10 +123,6 @@ void relocation(region_t* vector_addr, uint32_t src_address) {
             movw_calculate(relocation_info + i);
         } else if (relocation_info[i].type == 6) { // absoulately address: mowt
             movt_calculate(relocation_info + i);
-        } else if (relocation_info[i].type == 55) { // function pointer: moww
-            fp_movt_calculate(relocation_info + i);
-        }else if (relocation_info[i].type == 66) { // function pointer: mowt
-            fp_movt_calculate(relocation_info + i);
         }else {
         }
     }
